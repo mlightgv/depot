1fce9a66a17ae6088f1b7cb424976ecb765d0f4e
Mary Gomez Villarreal <marygomez@192-168-1-2.tpgi.com.au>
2015-06-20 15:15:59 +1000

    fix routes variable store_url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :line_items
   resources :carts
-  get 'store/index'
+  get 'store/index', as: 'store'
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
