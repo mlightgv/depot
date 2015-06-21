d3d98520856565f95311ee932ee2d9c95bdd5670
Mary Gomez Villarreal <marygomez@192-168-1-6.tpgi.com.au>
2015-06-21 21:00:21 +1000

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
-  get 'admin/index'
 
-  get 'sessions/new'
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  get "sessions/create"
+  get "sessions/destroy"
 
   resources :users
   resources :orders
   resources :line_items
   resources :carts
+
+  get "store/index"
   resources :products do
     get :who_bought, on: :member
   end
